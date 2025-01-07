6324c875c0f0de9356d6959f325fdf703ff9583a
NitinTheGreat <nitinpandey1304@gmail.com>
2025-01-07 11:58:48 +0530

    fix: replace user icon with star in notfound

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,11 +3,11 @@
 import { useState, useEffect } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import Link from 'next/link'
-import { Rocket, Satellite, User as  Star, Zap } from 'lucide-react'
+import { Rocket, Satellite, Star as  Star, Zap } from 'lucide-react'
 
 export default function Custom404() {
   const [text, setText] = useState('')
-  const fullText = "Hoouston, we have a problem. The page you're looking for has floated away into space."
+  const fullText = ", we have a problem. The page you're looking for has floated away into space."
   const [showStars, setShowStars] = useState(false)
 
   useEffect(() => {
