1f14a94e4eb5831d8de2b1d7d0004199e2aa051c
Nitin Kumar Pandey <83699589+NitinTheGreat@users.noreply.github.com>
2025-01-07 13:09:08 +0530

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ import { Rocket, Satellite, Star as  Star, Zap } from 'lucide-react'
 
 export default function Custom404() {
   const [text, setText] = useState('')
-  const fullText = ", we have a problem. The page you're looking for has floated away into space."
+  const fullText = "Houston , we have a problem. The page you're looking for has floated away into space."
   const [showStars, setShowStars] = useState(false)
 
   useEffect(() => {
