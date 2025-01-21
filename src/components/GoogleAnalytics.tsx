caf11c483e2f8bc5789a5b186c216fe9fc428bd8
NitinTheGreat <nitinpandey1304@gmail.com>
2025-01-22 04:09:05 +0530

    fix: build

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -2,19 +2,22 @@
 
 import Script from "next/script"
 import { usePathname, useSearchParams } from "next/navigation"
-import { useEffect } from "react"
+import { useEffect, Suspense } from "react"
 
-// TODO: Replace 'G-CXHYSXSLXP' with your actual Google Analytics Measurement ID
-const GA_MEASUREMENT_ID = "G-CXHYSXSLXP"
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
 
 declare global {
   interface Window {
     gtag: any
     dataLayer: any[]
+    trackEvent: (eventName: string, eventParams?: object) => void
+    setUserProperties: (properties: object) => void
+    trackTiming: (category: string, variable: string, value: number, label?: string) => void
+    trackException: (description: string, fatal?: boolean) => void
   }
 }
 
-export function GoogleAnalytics() {
+function GoogleAnalyticsInner() {
   const pathname = usePathname()
   const searchParams = useSearchParams()
 
@@ -28,6 +31,10 @@ export function GoogleAnalytics() {
     }
   }, [pathname, searchParams])
 
+  return null
+}
+
+export function GoogleAnalytics() {
   return (
     <>
       <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} />
@@ -36,43 +43,46 @@ export function GoogleAnalytics() {
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-                        gtag('config', '${GA_MEASUREMENT_ID}', {
-                            page_path: window.location.pathname,
-                        });
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}', {
+                page_path: window.location.pathname,
+            });
 
-                        // Custom event tracking function
-                        window.trackEvent = function(eventName, eventParams = {}) {
-                            gtag('event', eventName, eventParams);
-                        };
+            // Custom event tracking function
+            window.trackEvent = function(eventName, eventParams = {}) {
+                gtag('event', eventName, eventParams);
+            };
 
-                        // Set user properties function
-                        window.setUserProperties = function(properties) {
-                            gtag('set', 'user_properties', properties);
-                        };
+            // Set user properties function
+            window.setUserProperties = function(properties) {
+                gtag('set', 'user_properties', properties);
+            };
 
-                        // Track timing function
-                        window.trackTiming = function(category, variable, value, label) {
-                            gtag('event', 'timing_complete', {
-                                name: variable,
-                                value: value,
-                                event_category: category,
-                                event_label: label
-                            });
-                        };
+            // Track timing function
+            window.trackTiming = function(category, variable, value, label) {
+                gtag('event', 'timing_complete', {
+                    name: variable,
+                    value: value,
+                    event_category: category,
+                    event_label: label
+                });
+            };
 
-                        // Track exception function
-                        window.trackException = function(description, fatal = false) {
-                            gtag('event', 'exception', {
-                                description: description,
-                                fatal: fatal
-                            });
-                        };
-                    `,
+            // Track exception function
+            window.trackException = function(description, fatal = false) {
+                gtag('event', 'exception', {
+                    description: description,
+                    fatal: fatal
+                });
+            };
+          `,
         }}
       />
+      <Suspense fallback={null}>
+        <GoogleAnalyticsInner />
+      </Suspense>
     </>
   )
 }
